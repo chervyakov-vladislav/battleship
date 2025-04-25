3c89145a679105fe2b8bbcdbfe50832de78546ce
Vladislav Chervyakov <chervyakovve@mail.ru>
2025-04-25 20:54:02 +0700

    feat: init websocket server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
+import { WebSocketServer } from 'ws';
 import { httpServer } from "./http_server/index";
 
 const HTTP_PORT = 8181;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
+const webSocketServer = new WebSocketServer({ port: 3000 },
+  () => console.log(`WebSocketServer is running on port: 3000!`)
+);
+
+webSocketServer.on('connection', (ws) => {
+  ws.on('close', async () => {
+    console.log(`Client disconnected`);
+  });
+
+  ws.on('message', (message) => {
+    console.log(JSON.parse(message.toString()));
+    ws.send(JSON.stringify('Приветик'))
+  });
+});
+
+webSocketServer.on('error ', (error) => {
+  console.log(`error ${error.message}`);
+});
+
+webSocketServer.on('close', () => {
+  console.log(`close`);
+});
+
+console.log(`Cleint started: http://localhost:${HTTP_PORT}`);
 httpServer.listen(HTTP_PORT);
